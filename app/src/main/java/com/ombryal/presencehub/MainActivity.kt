151849bf16ca0acedbc63ba3ecf8ba3f4c624301
OmbryalDev <naickerprajval47@gmail.com>
2026-07-10 12:37:42 +1200

    Add MainActivity for PresenceHub application

---
+++ app/src/main/java/com/ombryal/presencehub/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.ombryal.presencehub
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import com.ombryal.presencehub.ui.theme.PresenceHubTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            PresenceHubTheme {
+                PresenceHubApp()
+            }
+        }
+    }
+}
