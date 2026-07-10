3882bade831ea78ca1e2d117dc89753d2a1d4de9
OmbryalDev <naickerprajval47@gmail.com>
2026-07-10 13:03:02 +1200

    Initialize PluginManager and RPCManager in app

--- app/src/main/java/com/ombryal/presencehub/DiscordRPCHubApp.kt
+++ app/src/main/java/com/ombryal/presencehub/DiscordRPCHubApp.kt
@@ -1,5 +1,21 @@
 package com.ombryal.presencehub
 
 import android.app.Application
+import com.ombryal.presencehub.plugins.PluginManager
+import com.ombryal.presencehub.rpc.RPCManager
 
-class DiscordRPCHubApp : Application()
+class DiscordRPCHubApp : Application() {
+
+    lateinit var pluginManager: PluginManager
+        private set
+
+    lateinit var rpcManager: RPCManager
+        private set
+
+    override fun onCreate() {
+        super.onCreate()
+
+        pluginManager = PluginManager()
+        rpcManager = RPCManager(pluginManager)
+    }
+}
