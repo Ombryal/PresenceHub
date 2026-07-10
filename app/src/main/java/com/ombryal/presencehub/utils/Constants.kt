1ec730e210d963c1de6756b8486ebf1f950153ab
OmbryalDev <naickerprajval47@gmail.com>
2026-07-10 12:40:34 +1200

    Add Constants object with application constants

---
+++ app/src/main/java/com/ombryal/presencehub/utils/Constants.kt
@@ -0,0 +1,7 @@
+package com.ombryal.presencehub.utils
+
+object Constants {
+    const val APP_NAME = "PresenceHub"
+    const val PACKAGE_NAME = "com.ombryal.presencehub"
+    const val DEFAULT_UPDATE_INTERVAL_MS = 5000L
+}
