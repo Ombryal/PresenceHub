ae95e1b583f31fb57a7398e62da3b8e7a7d7e2be
OmbryalDev <naickerprajval47@gmail.com>
2026-07-10 20:11:49 +1200

    Update Constants.kt

--- app/src/main/java/com/ombryal/presencehub/utils/Constants.kt
+++ app/src/main/java/com/ombryal/presencehub/utils/Constants.kt
@@ -3,7 +3,12 @@ package com.ombryal.presencehub.utils
 object Constants {
     const val APP_NAME = "PresenceHub"
     const val PACKAGE_NAME = "com.ombryal.presencehub"
+
     const val DEFAULT_UPDATE_INTERVAL_MS = 5000L
 
-    const val PLUGIN_INDEX_URL = "https://raw.githubusercontent.com/Ombryal/PresenceHub/main/plugin-index.json"
+    const val PLUGIN_REPO_BASE_URL =
+        "https://raw.githubusercontent.com/Ombryal/PresenceHub-Plugins/main"
+
+    const val PLUGIN_INDEX_URL =
+        "$PLUGIN_REPO_BASE_URL/plugin-index.json"
 }
