7c59ea5189ae30a43f8484a1facb11e53e4631c5
OmbryalDev <naickerprajval47@gmail.com>
2026-07-10 12:38:28 +1200

    Add PresenceHubApp composable function

---
+++ app/src/main/java/com/ombryal/presencehub/PresenceHubApp.kt
@@ -0,0 +1,22 @@
+package com.ombryal.presencehub
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun PresenceHubApp() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = "PresenceHub",
+            style = MaterialTheme.typography.headlineMedium
+        )
+    }
+}
