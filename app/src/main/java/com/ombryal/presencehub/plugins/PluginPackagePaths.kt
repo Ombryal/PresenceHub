9b3b25072130bd7a60453b0bcf15ef5bac0af640
OmbryalDev <naickerprajval47@gmail.com>
2026-07-10 21:34:44 +1200

    Add PluginPackagePaths object with file constants

---
+++ app/src/main/java/com/ombryal/presencehub/plugins/PluginPackagePaths.kt
@@ -0,0 +1,8 @@
+package com.ombryal.presencehub.plugins
+
+object PluginPackagePaths {
+    const val INDEX_FILE = "plugin-index.json"
+    const val MANIFEST_FILE = "manifest.json"
+    const val CHECKSUM_FILE = "checksum.sha256"
+    const val SIGNATURE_FILE = "signature.sig"
+}
