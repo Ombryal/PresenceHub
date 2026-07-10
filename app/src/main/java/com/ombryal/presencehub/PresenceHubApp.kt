a4eb792016e9b693c150c08a24cc670d37a93384
OmbryalDev <naickerprajval47@gmail.com>
2026-07-10 13:46:05 +1200

    Modify PresenceHubApp to accept RPC callbacks

--- app/src/main/java/com/ombryal/presencehub/PresenceHubApp.kt
+++ app/src/main/java/com/ombryal/presencehub/PresenceHubApp.kt
@@ -4,6 +4,12 @@ import androidx.compose.runtime.Composable
 import com.ombryal.presencehub.navigation.AppNavigation
 
 @Composable
-fun PresenceHubApp() {
-    AppNavigation()
+fun PresenceHubApp(
+    onStartRpc: () -> Unit,
+    onStopRpc: () -> Unit
+) {
+    AppNavigation(
+        onStartRpc = onStartRpc,
+        onStopRpc = onStopRpc
+    )
 }
