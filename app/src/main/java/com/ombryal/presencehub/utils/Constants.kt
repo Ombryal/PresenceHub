f4439563d971c2604ad80034c389748657d4494e
OmbryalDev <naickerprajval47@gmail.com>
2026-07-10 13:48:43 +1200

    Add PLUGIN_INDEX_URL constant to Constants.kt

--- app/src/main/java/com/ombryal/presencehub/utils/Constants.kt
+++ app/src/main/java/com/ombryal/presencehub/utils/Constants.kt
@@ -4,4 +4,6 @@ object Constants {
     const val APP_NAME = "PresenceHub"
     const val PACKAGE_NAME = "com.ombryal.presencehub"
     const val DEFAULT_UPDATE_INTERVAL_MS = 5000L
+
+    const val PLUGIN_INDEX_URL = "https://raw.githubusercontent.com/Ombryal/PresenceHub/main/plugin-index.json"
 }
