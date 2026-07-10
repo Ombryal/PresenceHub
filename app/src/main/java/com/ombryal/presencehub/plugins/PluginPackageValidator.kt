9f6fe1e55ce7c0ff961e5f32edf9d744a4175286
OmbryalDev <naickerprajval47@gmail.com>
2026-07-10 21:34:13 +1200

    Add PluginPackageValidator for plugin compatibility checks

---
+++ app/src/main/java/com/ombryal/presencehub/plugins/PluginPackageValidator.kt
@@ -0,0 +1,12 @@
+package com.ombryal.presencehub.plugins
+
+object PluginPackageValidator {
+
+    fun isCompatible(manifest: PluginPackageManifest): Boolean {
+        return manifest.pluginId.isNotBlank() &&
+                manifest.name.isNotBlank() &&
+                manifest.version.isNotBlank() &&
+                manifest.apiVersion > 0 &&
+                manifest.packageUrl.isNotBlank()
+    }
+}
