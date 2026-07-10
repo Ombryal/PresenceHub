b916de9bbb469a4b42520ade73b0de595d000a5d
OmbryalDev <naickerprajval47@gmail.com>
2026-07-10 21:01:42 +1200

    Add storeState parameter to PresenceHubApp function

--- app/src/main/java/com/ombryal/presencehub/PresenceHubApp.kt
+++ app/src/main/java/com/ombryal/presencehub/PresenceHubApp.kt
@@ -3,17 +3,18 @@ package com.ombryal.presencehub
 import androidx.compose.runtime.Composable
 import com.ombryal.presencehub.navigation.AppNavigation
 import com.ombryal.presencehub.plugins.PluginRegistryEntry
+import com.ombryal.presencehub.plugins.PluginStoreState
 
 @Composable
 fun PresenceHubApp(
-    availablePlugins: List<PluginRegistryEntry>,
+    storeState: PluginStoreState,
     onRefreshPlugins: () -> Unit,
     onInstallPlugin: (PluginRegistryEntry) -> Unit,
     onStartRpc: () -> Unit,
     onStopRpc: () -> Unit
 ) {
     AppNavigation(
-        availablePlugins = availablePlugins,
+        storeState = storeState,
         onRefreshPlugins = onRefreshPlugins,
         onInstallPlugin = onInstallPlugin,
         onStartRpc = onStartRpc,
