6e6d9311dd8753ceedc6b833fdce26a7120223a3
OmbryalDev <naickerprajval47@gmail.com>
2026-07-10 20:40:22 +1200

    Add plugin management parameters to PresenceHubApp

--- app/src/main/java/com/ombryal/presencehub/PresenceHubApp.kt
+++ app/src/main/java/com/ombryal/presencehub/PresenceHubApp.kt
@@ -2,13 +2,20 @@ package com.ombryal.presencehub
 
 import androidx.compose.runtime.Composable
 import com.ombryal.presencehub.navigation.AppNavigation
+import com.ombryal.presencehub.plugins.PluginRegistryEntry
 
 @Composable
 fun PresenceHubApp(
+    availablePlugins: List<PluginRegistryEntry>,
+    onRefreshPlugins: () -> Unit,
+    onInstallPlugin: (PluginRegistryEntry) -> Unit,
     onStartRpc: () -> Unit,
     onStopRpc: () -> Unit
 ) {
     AppNavigation(
+        availablePlugins = availablePlugins,
+        onRefreshPlugins = onRefreshPlugins,
+        onInstallPlugin = onInstallPlugin,
         onStartRpc = onStartRpc,
         onStopRpc = onStopRpc
     )
