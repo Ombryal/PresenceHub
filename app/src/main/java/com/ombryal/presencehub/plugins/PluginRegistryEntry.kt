b7c37b0c5adf4d6186eeb591e7dde8a13f7134fb
OmbryalDev <naickerprajval47@gmail.com>
2026-07-10 13:42:41 +1200

    Add PluginRegistryEntry data class

---
+++ app/src/main/java/com/ombryal/presencehub/plugins/PluginRegistryEntry.kt
@@ -0,0 +1,12 @@
+package com.ombryal.presencehub.plugins
+
+data class PluginRegistryEntry(
+    val pluginId: String,
+    val name: String,
+    val version: String,
+    val apiVersion: Int,
+    val downloadUrl: String,
+    val checksumSha256: String? = null,
+    val description: String? = null,
+    val verified: Boolean = false
+)
