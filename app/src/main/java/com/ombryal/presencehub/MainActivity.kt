a4ca5bcdaf0d53b89afd3a00e82952d255fd5df4
OmbryalDev <naickerprajval47@gmail.com>
2026-07-10 20:52:12 +1200

    Add PluginInstallManager to MainActivity

--- app/src/main/java/com/ombryal/presencehub/MainActivity.kt
+++ app/src/main/java/com/ombryal/presencehub/MainActivity.kt
@@ -3,6 +3,7 @@ package com.ombryal.presencehub
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import com.ombryal.presencehub.plugins.PluginInstallManager
 import com.ombryal.presencehub.plugins.PluginStore
 import com.ombryal.presencehub.service.ServiceController
 import com.ombryal.presencehub.ui.theme.PresenceHubTheme
@@ -10,6 +11,7 @@ import com.ombryal.presencehub.ui.theme.PresenceHubTheme
 class MainActivity : ComponentActivity() {
 
     private val pluginStore = PluginStore()
+    private val pluginInstallManager = PluginInstallManager()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,8 +26,7 @@ class MainActivity : ComponentActivity() {
                         pluginStore.refresh()
                     },
                     onInstallPlugin = { plugin ->
-                        // Install flow will get more real later.
-                        // For now this is just a placeholder action.
+                        pluginInstallManager.install(plugin)
                     },
                     onStartRpc = { ServiceController.startRpcService(this) },
                     onStopRpc = { ServiceController.stopRpcService(this) }
