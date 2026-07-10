87b461ae35cfb3e2f4ea56db56a7a4b9218dacb5
OmbryalDev <naickerprajval47@gmail.com>
2026-07-10 13:02:19 +1200

    Refactor RPCManager to manage presence updates
    Refactor RPCManager to include PluginManager and update presence handling.

--- app/src/main/java/com/ombryal/presencehub/rpc/RPCManager.kt
+++ app/src/main/java/com/ombryal/presencehub/rpc/RPCManager.kt
@@ -1,18 +1,39 @@
 package com.ombryal.presencehub.rpc
 
 import com.ombryal.presencehub.data.models.Presence
+import com.ombryal.presencehub.plugins.PluginManager
 
-class RPCManager {
+class RPCManager(
+    private val pluginManager: PluginManager
+) {
 
     private var currentPresence: Presence? = null
 
-    fun updatePresence(presence: Presence) {
-        currentPresence = presence
+    fun refreshPresence() {
+        val newPresence = pluginManager.getCurrentPresence()
+
+        if (newPresence != currentPresence) {
+            currentPresence = newPresence
+
+            if (newPresence != null) {
+                sendToDiscord(newPresence)
+            } else {
+                clearDiscordPresence()
+            }
+        }
     }
 
     fun clearPresence() {
         currentPresence = null
+        clearDiscordPresence()
     }
 
-    fun getCurrentPresence(): Presence? = currentPresence
+    private fun sendToDiscord(presence: Presence) {
+        // TODO: Connect this to the real Discord RPC implementation later.
+        // This is only the app-side bridge for now.
+    }
+
+    private fun clearDiscordPresence() {
+        // TODO: Clear Discord activity here.
+    }
 }
