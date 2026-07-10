e2577e16e3c54d93b4b49e504e6696c1c3e939d8
OmbryalDev <naickerprajval47@gmail.com>
2026-07-10 21:39:29 +1200

    Add PACKAGE_FILE constant for plugin APK

--- app/src/main/java/com/ombryal/presencehub/plugins/PluginPackagePaths.kt
+++ app/src/main/java/com/ombryal/presencehub/plugins/PluginPackagePaths.kt
@@ -5,4 +5,5 @@ object PluginPackagePaths {
     const val MANIFEST_FILE = "manifest.json"
     const val CHECKSUM_FILE = "checksum.sha256"
     const val SIGNATURE_FILE = "signature.sig"
+    const val PACKAGE_FILE = "plugin.apk"
 }
